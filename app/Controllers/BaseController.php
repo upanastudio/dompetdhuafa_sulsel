b54b39917148848d72b26d739516bdbea6362db2
Ahmad Irfandi <ahmad7irfandi@gmail.com>
2020-04-27 17:11:58 +0800

    Update upload helper

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -27,7 +27,7 @@ class BaseController extends Controller
      *
      * @var array
      */
-    protected $helpers = ['url', 'form', 'text', 'my_url_helper'];
+    protected $helpers = ['url', 'form', 'text', 'my_url_helper', 'upload_helper'];
 
     /**
      * Constructor.
